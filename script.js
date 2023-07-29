7747298d8d795d85cc21f5c11248178d8cc5f58a
Mahima Makode <140474655+MahimaMakode@users.noreply.github.com>
2023-07-29 14:54:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+gsap.from("#main2 .text2",{
+    y:100,
+    duration:.1,
+    opacity:1,
+    scrollTrigger:{
+        trigger:"#main2  .text2",
+        scroller:"body",
+        scrub:2,
+        // markers:true,
+        start:"top 90%",
+        end:"top 90%",
+    }
+}
+
+)
+gsap.from("#main2 #text .text1",{
+    y:100,
+    duration:.1,
+    opacity:1,
+    scrollTrigger:{
+        trigger:"#main2 #text .text1",
+        scroller:"body",
+        scrub:2,
+        // markers:true,
+        start:"top 90%",
+        end:"top 80%",
+    }
+}
+
+)
+gsap.from("#main2 .ang",{
+    y:50,
+    duration:.1,
+    scrollTrigger:{
+        trigger:"#main2 .ang",
+        scroller:"body",
+        scrub:2,
+        // markers:true,
+        start:"top 90%",
+        end:"top 90%",
+    }
+}
+
+)
+gsap.from("#main3 .text3",{
+    y:100,
+    duration:.1,
+    opacity:1,
+    scrollTrigger:{
+        trigger:"#main3  .text3",
+        scroller:"body",
+        scrub:2,
+        // markers:true,
+        start:"top 90%",
+        end:"top 60%",
+    }
+}
+)
+gsap.from("#main3 #text4 .text5",{
+    y:100,
+    duration:.1,
+    opacity:1,
+    scrollTrigger:{
+        trigger:"#main3 #text4 .text5",
+        scroller:"body",
+        scrub:2,
+        // markers:true,
+        start:"top 90%",
+        end:"top 60%",
+    }
+}
+)
+gsap.from("#main3 .ang",{
+    y:50,
+    duration:.1,
+    scrollTrigger:{
+        trigger:"#main3 .ang",
+        scroller:"body",
+        scrub:2,
+        // markers:true,
+        start:"top 90%",
+        end:"top 90%",
+    }
+}
+)
